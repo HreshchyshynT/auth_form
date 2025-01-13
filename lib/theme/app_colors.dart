324eed57b1c0ce627ae6a2cf310216de2364c64d
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2025-01-13 23:34:46 +0200

    add fonts, some colors, strings

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+
+class AppColors {
+  AppColors._();
+  static const darkBlue = Color(0xFF4A4E71);
+  static const darkBlueAlpha20 = Color(0x151D5133);
+}
---
+++ lib/theme/app_strings.dart
@@ -0,0 +1,13 @@
+final class AppStrings {
+  AppStrings._();
+
+  static const authTitle = "Sign up";
+  static const emailHint = "Email";
+  static const passwordHint = "Create your password";
+  static const signUpButton = "Sign up";
+  static const emailError = "Invalid email address";
+
+  static const passwordMinLengthHint = "8 characters or more (no spaces)";
+  static const passwordCaseHint = "Uppercase and lowercase letters";
+  static const passwordDigitHint = "At least one digit";
+}
